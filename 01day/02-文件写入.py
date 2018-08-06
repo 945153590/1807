c20488151ec2ff1694748544077e7a8cd20f25b4
NickName <945153590@qq.com>
2018-08-06 14:16:38 +0800

    daima

---
+++ 01day/02-文件写入.py
@@ -0,0 +1,3 @@
+f = open('1.txt','w')
+f.write('hello world\nhello world\nhello world')
+f.close()
---
+++ 01day/05-名片系统持久化.py
@@ -0,0 +1,15 @@
+list = []
+d = {}
+def add():
+	name = input('请输入姓名：')
+	sex = input('请输入性别：')
+	age = input('请输入年龄：')
+	d['name'] = name
+	d['sex'] = sex
+	d['age'] = age
+	list.append(d)
+def savecard():
+	pass
+def readcard():
+	pass
+add()
